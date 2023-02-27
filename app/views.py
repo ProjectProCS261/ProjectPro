c911fd1958ca3ba5ec3f7fd82f481d988ec841a7
gcbn <74546980+gcbn@users.noreply.github.com>
2023-02-27 19:03:14 +0000

    Set up flask-login; users can now log in & log out

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, flash, jsonify, render_template, request, redirect, session, url_for
-from flask_login import LoginManager
+from flask_login import login_user, LoginManager, logout_user, current_user, login_required, UserMixin
 
 import pymongo
 
@@ -9,152 +9,103 @@ from markupsafe import escape
 
 from app import app
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # Connect to the MongoDB database
 client = pymongo.MongoClient("mongodb+srv://Brian:Kdh010315@cluster0.qmtgd2o.mongodb.net/test")
 db = client["cs261"]
 users = db["USER"]
 
-# @app.route('/')
-# def index():
-#     # if current_user.is_authenticated:
-#     #     return redirect(url_for('home'))  # Redirect to logged-in default page
-#     return render_template('nauth/register.html')
+# User class for use by Flask-Login 
+class User(UserMixin):
+    def __init__(self, useremail):
+        self.useremail = useremail
 
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     # if current_user.is_authenticated:
-#     #     return redirect(url_for('home'))
-#     if request.method == 'POST':
-#         username = request.form.get('username')
-#         password = request.form.get('password')
-#         # # Get data from mongoDB and check
-#         # userdata = User.query.filter_by(username=username).first()
-#         # if userdata is None:  # If User with entered username not in database, then return error.
-#         #     return jsonify(
-#         #         {
-#         #             "success": 0,
-#         #         }
-#         #     )
-#         # else:
-#         #     hashpass = userdata.password_hash
-#         #     check = werkzeug.security.check_password_hash(
-#         #         hashpass, password)  # Check Password
-#         #     if check:
-#         #         # Login
-#         #         login_user(userdata)
-#         #         return redirect(url_for('home'))
-#         #     else:
-#         #         return jsonify(
-#         #             {
-#         #                 "success": 0,
-#         #             }
-#         #         )
-
-#     return render_template('nauth/login.html')
-
-
-# @app.route('/register',  methods=['GET', 'POST'])
-# def register():
-#     # if current_user.is_authenticated:
-#     #     return redirect(url_for('home'))
-#     if request.method == 'POST':
-#         name = request.form.get('name')
-#         # Make sure username uniqueness doesn't get confusing.
-#         username = request.form.get('username').lower()
-#         password = request.form.get('password')
-#         if len(password) < 8:  # Secure Password
-#             return jsonify(
-#                 {
-#                     "length": 0,
-#                 }
-#             )
-#         password_hash = werkzeug.security.generate_password_hash(
-#             password, method='pbkdf2:sha256', salt_length=16)
-#         userdata = None
-#         if userdata is not None:  # If username is taken return error message
-#             return jsonify(
-#                 {
-#                     "unique": 0,
-#                 }
-#             )
-        
-#         # Insert new User into db
-
-#         # Login
-#         # login_user(userdata)
-#         # return redirect(url_for('home'))
-#     return render_template('nauth/register.html')
-
-
-# @app.route('/home')
-# @login_required
-# def home():
-#     return redirect(url_for('main'))
-
-
-# @ app.route('/logout')
-# def logout():
-#     logout_user()
-#     return redirect(url_for('index'))
-
-
-# @ login_manager.user_loader
-# def load_user(user_id):
-#     try:
-#         # Change to MongoDB
-#         return User.query.filter_by(id=user_id).first()
-#     except User.DoesNotExist:
-#         return None
-
-
-# @ login_manager.unauthorized_handler
-# def unauthorized_handler():
-#     return redirect(url_for('index'))
-
+    def get_id(self):
+        return self.useremail
 
 @app.route('/')
 def index():
+    print(current_user.is_authenticated)
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))  # Redirect to logged-in default page
     return render_template('nauth/register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home')) 
     if request.method == 'POST':
         # Check if the username and password are valid
-        user = users.find_one({'email': request.form['email']})
-        if user and user['password'] == request.form['password']:
-            return render_template('auth/home.html')
-        else:
-            return render_template('nauth/login.html', error='Invalid username or password')
+        userdata = users.find_one({'email': request.form['email'].lower()})
+        if userdata:
+            hashpass = userdata['passwordhash']
+            check = werkzeug.security.check_password_hash(hashpass, request.form['password'])
+            if check:
+                loginUser = User(userdata['email'])
+                # Login User
+                login_user(loginUser)
+                return redirect(url_for('home'))
+            else:
+                return render_template('nauth/login.html', error='Invalid username or password')
 
     return render_template('nauth/login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home')) 
     if request.method == 'POST':
         # Check if the username already exists
-        if users.find_one({'email': request.form['email']}):
-            return render_template('register.html', error='Username already taken')
+        if users.find_one({'email': request.form['email'].lower()}):
+            return render_template('nauth/register.html', error='Email already in use')
         
         # Check if the password and confirm password match
         if request.form['password'] != request.form['confirm_password']:
-            return render_template('register.html', error='Passwords do not match')
+            return render_template('nauth/register.html', error='Passwords do not match')
 
+        password_hash = werkzeug.security.generate_password_hash(request.form['password'], method='pbkdf2:sha256', salt_length=16)
 
         # Create a new user
-        user = {
-            'email': request.form['email'],
-            'password': request.form['password']
+        userdata = {
+            'email': request.form['email'].lower(),
+            'passwordhash': password_hash
         }
-        users.insert_one(user)
+        users.insert_one(userdata)
+        loginUser = User(userdata['email'])
+        # Login User
+        login_user(loginUser)
 
         return redirect(url_for('login'))
 
     return render_template('nauth/register.html')
 
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    # Logout User
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/addproject')
+@login_required 
 def addProject():
     return render_template('auth/addProject.html')
+
+@app.route('/home')
+@login_required
+def home():
+    return render_template('auth/home.html')
+
+@login_manager.user_loader
+def load_user(useremail):
+    userdata = users.find_one({'email': useremail})
+    print(userdata)
+    if userdata:
+        return User(userdata['email'])
+    else:
+        return None
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return redirect(url_for('index'))
